73d831f0fb104337d98520a2a50b3261e98cb2db
blakehartman <bhartman@clickrain.com>
2022-11-26 07:21:25 -0700

    fine-tuned base

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,6 +1,4 @@
 import type { ActionArgs, LoaderArgs } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
-
 import { logout } from "~/session.server";
 
 export async function action({ request }: ActionArgs) {
