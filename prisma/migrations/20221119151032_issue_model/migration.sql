ad35cefe22d377ab10545468491132d1496a6097
blakehartman <bhartman@clickrain.com>
2022-11-26 08:23:54 -0700

    cleaned up db migrations

--- prisma/migrations/20221119151032_issue_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Issue" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "summary" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    "projectId" TEXT NOT NULL,
-    CONSTRAINT "Issue_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
